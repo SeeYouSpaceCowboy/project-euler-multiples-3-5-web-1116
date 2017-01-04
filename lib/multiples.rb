3676c07574c3ccf33930ce885617ca93de0cbbc6
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-01-04 11:47:09 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  range = (1..(limit-1))
+
+  range.collect do |num|
+    if num%3 == 0 || num%5 == 0
+      num
+    end
+  end.compact
+end
+
+def sum_multiples(limit)
+  sum = 0
+
+  collect_multiples(limit).each do |num|
+    sum += num
+  end
+
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    range = (1..(limit-1))
+
+    range.collect do |num|
+      if num%3 == 0 || num%5 == 0
+        num
+      end
+    end.compact
+  end
+
+  def sum_multiples
+    sum = 0
+
+    collect_multiples.each do |num|
+      sum += num
+    end
+
+    sum
+  end
+end
